8c3fdfee4f8f8e21a50d60d6ff535668c7cf9910
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2021-09-02 11:26:32 +0530

    Update settings.py

--- Api_Crawler/settings.py
+++ Api_Crawler/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
